336ea9ecc61e38b5875371ae85aa4fa691170f2c
amarafiif <amarafif230@gmail.com>
2024-02-26 19:43:22 +0700

    remastering style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        Rubik: ['Rubik Glitch'],
-        Kanit: ['Kanit'],
-       },
-    },
-  },
-  plugins: [],
-}
+	darkMode: 'class',
+	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				Rubik: ['Rubik Glitch'],
+				Kanit: ['Kanit'],
+				Chakra: ['Chakra Petch'],
+			},
+		},
+	},
+	plugins: [],
+};
