84219a83e41820f84c3f84bcd07da3d77a18466d
amarafiif <amarafif230@gmail.com>
2024-03-13 01:08:12 +0700

    fix: navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
-import Head from 'next/head';
-import { BsFillMoonStarsFill, BsMedium } from 'react-icons/bs';
-import { AiFillInstagram, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai';
-import { FaPhp, FaFigma } from 'react-icons/fa';
-import { SiTailwindcss, SiMysql } from 'react-icons/si';
-import Image from 'next/image';
-import profile from '../public/profile-avatar.png';
-import { useState } from 'react';
-import Typewriter from 'typewriter-effect';
+import Head from "next/head";
+import { BsFillMoonStarsFill, BsMedium } from "react-icons/bs";
+import { AiFillInstagram, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
+import { FaPhp, FaFigma } from "react-icons/fa";
+import { SiTailwindcss, SiMysql } from "react-icons/si";
+import Image from "next/image";
+import profile from "../public/profile-avatar.png";
+import { useState } from "react";
+import Typewriter from "typewriter-effect";
 
 export default function Home() {
 	const [darkMode, setDarkMode] = useState(true);
 	return (
-		<div className={darkMode ? 'dark' : ''}>
+		<div className={darkMode ? "dark" : ""}>
 			<Head>
 				<title>Muhammad Ammar Afif - Portfolio</title>
 				<meta name="description" content="Ini adalah Portfolio Muhammad Ammar Afif" />
@@ -23,7 +23,8 @@ export default function Home() {
 					{/* Sebelah Kiri */}
 					<div className="flex flex-col justify-space-between">
 						<nav className="py-10 mb-12 flex justify-between sticky top-0 px-10 md:px-20 lg:px-32 backdrop-blur-3xl bg-slate z-10">
-							<h1 className="text-4xl font-bold text-indigo-500">Portfolio</h1>
+							<h1 className="text-4xl font-bold text-indigo-500 hidden md:block">Portfolio</h1>
+							<Image src={profile} width={80} height={80} className="rounded-full border-2 border-indigo-500 md:hidden" alt={"Avatar"} />
 							<ul className="flex items-center">
 								<li>
 									<BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl dark:text-white" />
@@ -41,17 +42,14 @@ export default function Home() {
 								<h3 className="text-xl font-medium text-slate-700 md:text-4xl dark:text-slate-400">
 									<Typewriter
 										options={{
-											strings: ['Backend Web Enthusiast'],
+											strings: ["Backend Web Enthusiast"],
 											autoStart: true,
 											loop: true,
 										}}
 									/>
 								</h3>
-								<p className="text-xl py-4 mt-10 text-gray-600 md:text-2xl max-w-2xl dark:text-gray-400">
-									I am an undergraduate student in Informatics who likes to study website programming and web design. I have worked on UI/UX projects and web
-									development several times as a freelance. I really hope I can work with you. 👋
-								</p>
-								<div className="flex justify-left gap-6 lg:gap-12 mt-8 text-xl md:text-2xl lg:text-4xl text-indigo-500">
+								<p className="text-xl py-4 mt-10 text-gray-600 md:text-2xl max-w-2xl dark:text-gray-400">I am an undergraduate student in Informatics who likes to study website programming and web design. I have worked on UI/UX projects and web development several times as a freelance. I really hope I can work with you. 👋</p>
+								<div className="flex justify-left gap-6 lg:gap-12 mt-8 text-3xl md:text-4xl lg:text-4xl text-indigo-500">
 									<a href="https://www.instagram.com/amarafiif" className="hover:text-indigo-600" target="_blank">
 										<AiFillInstagram />
 									</a>
@@ -69,7 +67,7 @@ export default function Home() {
 
 							{/* Sebelah Kanan */}
 							<div className="relative mt-10 hidden md:block md:mt-18 ml-auto bg-gradient-to-r from-cyan-500 to-indigo-500 w-60 h-60 rounded-t-full overflow-hidden lg:h-[100%] lg:w-[70%] ring-2 ring-indigo-500 ring-offset-4 ring-offset-slate-50 dark:ring-offset-slate-900">
-								<Image src={profile} layout="fill" objectFit="cover" alt={'Avatar'} />
+								<Image src={profile} layout="fill" objectFit="cover" alt={"Avatar"} />
 							</div>
 						</div>
 					</div>
@@ -96,10 +94,7 @@ export default function Home() {
 								<h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
 									<a href="https://github.com/amarafiif/Holocation.git">Holocation - Ticketing Destination Web Application</a>
 								</h2>
-								<p className="mb-5 font-light text-gray-500 dark:text-gray-400">
-									A dynamic website used to display information on tourist destinations in Yogyakarta. This website also displays information on how much the
-									entrance ticket is to visit that destination and you can also order entrance tickets to that destination.
-								</p>
+								<p className="mb-5 font-light text-gray-500 dark:text-gray-400">A dynamic website used to display information on tourist destinations in Yogyakarta. This website also displays information on how much the entrance ticket is to visit that destination and you can also order entrance tickets to that destination.</p>
 								<div className="flex justify-between items-center">
 									<div className="flex items-center space-x-4">
 										<span className="text-slate-700 dark:text-slate-400">
@@ -115,10 +110,7 @@ export default function Home() {
 									<a href="https://github.com/amarafiif/Holocation.git" className="inline-flex items-center font-medium text-indigo-500 hover:text-indigo-700">
 										Repository
 										<svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-											<path
-												fillRule="evenodd"
-												d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-												clipRule="evenodd"></path>
+											<path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
 										</svg>
 									</a>
 								</div>
@@ -127,10 +119,7 @@ export default function Home() {
 								<div className="flex justify-between items-center mb-5 text-gray-500">
 									<span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
 										<svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-											<path
-												fillRule="evenodd"
-												d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
-												clipRule="evenodd"></path>
+											<path fillRule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clipRule="evenodd"></path>
 											<path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path>
 										</svg>
 										UI/UX Design
@@ -140,10 +129,7 @@ export default function Home() {
 								<h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
 									<a href="#">BengkelLine - Mobile App Design Bengkel Online</a>
 								</h2>
-								<p className="mb-5 font-light text-gray-500 dark:text-gray-400">
-									A mobile application design prototype designed to develop mobile-based online workshop applications. Where the application has many features
-									such as: towing vehicles, calling tire repairs, Displaying stalls that sell the nearest retail petrol and buying and selling spare parts.
-								</p>
+								<p className="mb-5 font-light text-gray-500 dark:text-gray-400">A mobile application design prototype designed to develop mobile-based online workshop applications. Where the application has many features such as: towing vehicles, calling tire repairs, Displaying stalls that sell the nearest retail petrol and buying and selling spare parts.</p>
 								<div className="flex justify-between items-center">
 									<div className="flex items-center space-x-4">
 										<span className="text-slate-700 dark:text-slate-400">
@@ -153,10 +139,7 @@ export default function Home() {
 									<a href="#" className="inline-flex items-center font-medium text-indigo-500 hover:text-indigo-700">
 										Prototype
 										<svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-											<path
-												fillRule="evenodd"
-												d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-												clipRule="evenodd"></path>
+											<path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
 										</svg>
 									</a>
 								</div>
